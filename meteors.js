2d431338a334f432bec269d7cb5c8f350e6e4096
John-Giovenco <JohnGiovenco93@gmail.com>
2023-03-25 14:35:26 -0700

    started meteor javascript

--- meteors.js
+++ meteors.js
@@ -1,1 +1,11 @@
-const meteors = document.querySelectorAll('.')
+function setUpMeteor () {
+    let meteorElement = document.querySelector('#meteor1')
+    meteorObject.element = meteorElement
+}
+
+const 
+
+function moveSouth(time) {
+    direction = 'south'
+    element.src = './assets/meteor1'
+}
